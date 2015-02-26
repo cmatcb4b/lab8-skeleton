c490300fcf42a3a6610a620f2988b396878661e3
vagrant <vagrant@precise32.(none)>
2015-02-26 20:24:47 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var projects = require("../projects.json")
  * GET home page.
  */
 
-exports.view = function(req, res){
-  // Here you should implement your random landing page rendering functionality
-  res.render("index", projects);
-};
+ exports.view = function(req, res){
+ 	var randomNumber = Math.random();
+
+ 	if (randomNumber > .5) {
+ 		res.render("index", projects);
+ 	} else {
+ 		res.render("index_alternate", projects)
+ 	}
+ };
